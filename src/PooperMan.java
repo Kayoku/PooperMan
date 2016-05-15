b4ea812b96cb413fb2e070771a2b21dd6fbe27e0
jordan <jordan.bouchoucha@gmail.com>
2016-05-15 17:45:17 +0200

    Premier commit.

---
+++ src/PooperMan.java
@@ -0,0 +1,18 @@
+import static org.lwjgl.glfw.GLFW.*;
+
+public class PooperMan {
+
+	public static void main(String[] args) {
+		glfwInit();
+		
+		long win = glfwCreateWindow(640, 480, "RainbowMan", 0, 0);
+		
+		glfwShowWindow(win);
+		
+		while(!glfwWindowShouldClose(win)) {
+			glfwPollEvents();
+		}
+		
+	}
+
+}
